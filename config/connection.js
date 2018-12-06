8d5b0d643850de2bdd29ff6cc666ed6c825e20af
sergeymaguire <sergeymaguire@gmail.com>
2018-12-06 12:14:40 -0800

    a bathroom finder

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+if(process.env.JAWSDB_URL){
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1525wilma",
+    database: "bathroom"
+  });
+}
+var PORT = process.env.PORT || 3000;
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+module.exports = connection;
